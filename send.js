0a10cc663500124efce52a635346a4db2ab1b5fc
TelecomsXChange <tier1@telecomsxchange.com>
2019-07-20 19:48:17 +0300

    Update send.js
    Updated script with billing receipt support and other minor improvements

--- send.js
+++ send.js
@@ -1,46 +1,60 @@
+const nanoid = require('nanoid')
+
 var smpp = require('smpp');
-var session = smpp.connect('smpp://smpp01.telecomsxchange.com:2776');
+var session = smpp.connect('smpp://staging.telecomsxchange.com:2776');
+
+smpp.addTLV('billing_price', {
+    id: 0x1520,
+    type: smpp.types.tlv.string
+});
+
+smpp.addTLV('billed_msgs_cnt', {
+    id: 0x1521,
+    type: smpp.types.tlv.string
+});
 
 
 
 session.bind_transceiver({
-	system_id: '{SMPP System_UD}',  // You can get credintials at https://members.telecomsxchange.com
-	password: '{SMPP Password}'
+	system_id: 'Username',
+	password: 'Password'
 }, function(pdu) {
-	if (pdu.command_status == 0) {
-		// Successfully bound
-		session.submit_sm({
-			//destination_addr: '86617#41799555221',   // Sample of sending message using different carrier
-			destination_addr: '19542400000',  // Sending SMS through Defualt Route.
-			source_addr: 'MNyCompany',
-			registered_delivery: 1,
-			short_message: 'Verification code is 13774'
-			//message_payload: 'text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text       !1 text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text       !2 text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text       !3'
-			
-		}, function(pdu) {
-			if (pdu.command_status == 0) {
-				// IF Message successfully sent
-				console.log("message sent OK");
-			} else {
-				console.log("message sending failed");
-			}
-			console.log(pdu);
-		});
+        if (pdu.command_status == 0) {
+                // Successfully bound
+                session.submit_sm({
+                        
+			destination_addr: '9647716568569',
+           
+                        //source_addr: 'O2App',
+                        source_addr: 'Alert',
+                        registered_delivery: 1,   
+                        message_id: nanoid(24),
+                        short_message:'Hello World' ,
+                        //short_message: 'text  text  text  text  text      '
+                        //message_payload: 'text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text       !1 text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text       !2 text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text       !3'
 
-		session.on('deliver_sm', function(pdu) {
-			console.log(pdu)
-	    		if (pdu.esm_class == 4) {
-	       			var shortMessage = pdu.short_message;
-	       			console.log('Received DR: %s', shortMessage);
-	       			session.send(pdu.response());
-	    		}
-	    	});
+                }, function(pdu) {
+                        if (pdu.command_status == 0) {
+                                // Message successfully sent
+                                console.log("message sent OK");
+                        } else {
+                                console.log("message sending failed");
+                        }
+                        console.log("SOMETHING HAPPENNED:",pdu);
+                });
+
+                session.on('deliver_sm', function(pdu) {
+                        console.log(pdu)
+                        if (pdu.esm_class == 4) {
+                                var shortMessage = pdu.short_message;
+                                console.log('Received DR: %s', shortMessage);
+                                session.send(pdu.response());
+                        }
+                });
 
                 session.on('pdu', function(pdu) {
                         console.log("GOT PDU ", pdu)
                 });
 
-
-
-	}
+        }
 });
