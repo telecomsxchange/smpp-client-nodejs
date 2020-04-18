dc63aed6195382466ada1c38198d012555b68799
TelecomsXChange <tier1@telecomsxchange.com>
2020-04-18 20:06:02 +0300

    Update send.js
    Added a little more guides to the code.

--- send.js
+++ send.js
@@ -3,10 +3,15 @@ const nanoid = require('nanoid')
 var smpp = require('smpp');
 var session = smpp.connect('smpp://staging.telecomsxchange.com:2776');
 
+// Parse the price per message from TCXC SMPP proxy and dsiplaye it in the response.
+
 smpp.addTLV('billing_price', {
     id: 0x1520,
     type: smpp.types.tlv.string
 });
+
+// Parse the count of messages from TCXC SMPP proxy and dsiplaye it in the response. 
+// Useful when sending UDH or concatenated messages.
 
 smpp.addTLV('billed_msgs_cnt', {
     id: 0x1521,
@@ -15,34 +20,39 @@ smpp.addTLV('billed_msgs_cnt', {
 
 
 
+
 session.bind_transceiver({
-	system_id: 'Username',
-	password: 'Password'
+	system_id: 'Username',      // You can generate from TCXC buyer portal under accounts page
+	password: 'Password'        // You can define in TCXC buyer portal under accounts page
 }, function(pdu) {
         if (pdu.command_status == 0) {
                 // Successfully bound
                 session.submit_sm({
                         
-			destination_addr: '9647716568569',
+			destination_addr: '9647716568569',       // The destination phone number you want to send a message to
            
-                        //source_addr: 'O2App',
-                        source_addr: 'Alert',
+                        //source_addr: 'O2App',                  // The Sender ID or Address, This will be displayed to the destination phone number
+                       
+			source_addr: 'Alert',
                         registered_delivery: 1,   
                         message_id: nanoid(24),
-                        short_message:'Hello World' ,
-                        //short_message: 'text  text  text  text  text      '
-                        //message_payload: 'text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text       !1 text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text       !2 text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text       !3'
+                        short_message:'Hello World' ,           // The message body, Replace it with the message you want to send.
+                        
+			//message_payload: 'text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text       !1 text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text       !2 text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text  text       !3'
 
                 }, function(pdu) {
                         if (pdu.command_status == 0) {
                                 // Message successfully sent
                                 console.log("message sent OK");
                         } else {
+				// Message failed to be sent
                                 console.log("message sending failed");
                         }
                         console.log("SOMETHING HAPPENNED:",pdu);
                 });
 
+		
+		// Print Delivery Receipt logs in terminal if DLR is received
                 session.on('deliver_sm', function(pdu) {
                         console.log(pdu)
                         if (pdu.esm_class == 4) {
